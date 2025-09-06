2d6254d90e3015849e92ff0a418d5ec6ef8866c7
The-One-and-Only-Caleb <shrapnca28@student.tcs.act.edu.au>
2025-09-06 20:48:28 +1000

    Added first powerup for purple bullet

--- Bullets/purple_bullet.gd
+++ Bullets/purple_bullet.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-
+var destroyed = 0
 const SPEED = 800.0
 
 
@@ -9,11 +9,16 @@ func _ready() -> void:
 	$Hurtbox.body_entered.connect(_on_hurtbox_body_entered)
 
 func _on_hurtbox_body_entered(_body):
-	$AreaofAffect/CollisionShape2D.disabled = false
-	queue_free()
+	if GameScript.purple_bullet_upgrades.has("Paint Splash") == true:
+		$AreaofAffect/CollisionShape2D.call_deferred("set_disabled", false)
+	destroyed += 1
 
 func _physics_process(delta: float) -> void:
 	if GameScript.paused == false:
 		velocity = Vector2.RIGHT.rotated(rotation) * SPEED
 		position += velocity * delta
 		move_and_slide()
+	if destroyed > 0:
+		destroyed += 1
+	if destroyed > 2:	
+		queue_free()
