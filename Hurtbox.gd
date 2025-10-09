07b44d0e583ff8e6aa8a69a6300472875cf5b169
The-One-and-Only-Caleb <shrapnca28@student.tcs.act.edu.au>
2025-10-09 16:45:26 +1100

    Fixed bug with hurtboxes and Arrow Caster

--- Hurtbox.gd
+++ Hurtbox.gd
@@ -2,16 +2,14 @@
 class_name HurtBox
 extends Area2D
 
-func _init() -> void:
-	collision_layer = 0
-	collision_mask = 2
-	
 func _ready() -> void:
 	self.area_entered.connect(_on_area_entered)
 
 func _on_area_entered(hitbox: HitBox):
 	if hitbox == null:
 		return
-		
+	print("Hitbox layer: ", hitbox.collision_layer)
+	print("Hurtbox mask: ", collision_mask)
+
 	if owner.has_method("take_damage"):
 		owner.take_damage(hitbox.damage, hitbox.global_position, hitbox.knockback)
